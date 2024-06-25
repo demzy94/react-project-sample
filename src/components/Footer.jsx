cec76ec44ac2e40e2794d58db71f34dd51363ca8
destinybravos <destinybravos@gmail.com>
2024-06-25 18:31:33 +0100

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+    return (
+        <footer className="min-h-96 bg-slate-800 text-white">
+            <div className="max-w-6xl mx-auto">
+                <h1>Footer</h1>
+            </div>
+        </footer>
+    )
+}
+
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <header className="min-h-96 bg-teal-400 text-white">
+            <div className="max-w-6xl mx-auto">
+                <h1>Header</h1>
+            </div>
+        </header>
+    )
+};
+
+export default Header;
